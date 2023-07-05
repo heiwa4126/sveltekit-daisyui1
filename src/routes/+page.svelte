d4179e2bcf23912c3aee2fd035d92e215c5e4152
heiwa4126 <heiwa4126@gmail.com>
2023-07-05 17:53:13 +0900

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script lang="ts">
+	import Counter from './Counter.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
+<Counter />
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ src/routes/Counter.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let count = 0;
+</script>
+
+<div class="counter">
+	<div>
+		<button on:click={() => (count -= 1)}>-</button>
+		{count}
+		<button on:click={() => (count += 1)}>+</button>
+	</div>
+</div>
