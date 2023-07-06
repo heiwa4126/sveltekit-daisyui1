fe3df03b05eb032d56ebf661c6808cb69d00c286
heiwa4126 <heiwa4126@gmail.com>
2023-07-06 14:17:52 +0900

    update for kit.paths.base things again (maybe work???)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,10 @@
 <script>
 	import { base } from '$app/paths';
-	import Link from '$components/Link.svelte';
 </script>
 
 <nav>
-	&lt;a&gt;
 	<a href="{base}/">home</a>
 	<a href="{base}/about">about</a>
-	&lt;Link&gt;
-	<Link href="/">home</Link>
-	<Link href="/about">about</Link>
 </nav>
 
 <slot />
