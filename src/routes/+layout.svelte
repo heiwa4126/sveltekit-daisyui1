cc07f1af9a76743382154d5d1d6d2c58cc3bf148
heiwa4126 <heiwa4126@gmail.com>
2023-07-11 17:27:31 +0900

    Add dark mode switch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,34 @@
 <script>
   import { base } from '$app/paths';
   import '../app.css';
+  let mode = 'light';
 </script>
 
 <svelte:head>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap" />
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,100..700,0..1,-50..200" />
 </svelte:head>
 
-<div data-theme="light" class="px-5 lg:px-10 prose max-w-none prose-sm md:prose-lg">
+<div data-theme={mode} class="px-5 lg:px-10 h-screen prose max-w-none prose-sm md:prose-lg">
   <nav>
     <a href="{base}/">home</a>
     <a href="{base}/blog">blog</a>
     <a href="{base}/about">about</a>
-    <button class="material-symbols-outlined symbols-fill baseline-tb">dark_mode</button>
+    <button
+      class="material-symbols-outlined symbols-fill micx"
+      on:click={() => {
+        mode = mode === 'dark' ? 'light' : 'dark';
+      }}>dark_mode</button
+    >
   </nav>
 
   <slot />
 </div>
+
+<style>
+  .micx {
+    font-size: 125%;
+    vertical-align: text-top;
+  }
+</style>
