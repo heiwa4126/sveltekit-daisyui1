5ddec7829848d55059fb110c432cb9d89f69fea2
heiwa4126 <heiwa4126@gmail.com>
2023-07-11 17:57:11 +0900

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,13 @@
   import { base } from '$app/paths';
   import '../app.css';
   let mode = 'light';
+  /**
+   * @param {string} mode1
+   */
+  function setMode(mode1) {
+    const htmlElement = document.querySelector('html');
+    htmlElement?.setAttribute('data-theme', mode1);
+  }
 </script>
 
 <svelte:head>
@@ -10,7 +17,7 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,100..700,0..1,-50..200" />
 </svelte:head>
 
-<div data-theme={mode} class="px-5 lg:px-10 h-screen prose max-w-none prose-sm md:prose-lg">
+<div class="px-5 lg:px-10 prose max-w-none prose-sm md:prose-lg">
   <nav>
     <a href="{base}/">home</a>
     <a href="{base}/blog">blog</a>
@@ -19,6 +26,7 @@
       class="material-symbols-outlined symbols-fill micx"
       on:click={() => {
         mode = mode === 'dark' ? 'light' : 'dark';
+        setMode(mode);
       }}>dark_mode</button
     >
   </nav>
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
+// import sveltePreprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 // for GitHub Pages
@@ -11,7 +12,7 @@ basepath = basepath ? '/' + basepath : '';
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: [vitePreprocess()],
 
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
